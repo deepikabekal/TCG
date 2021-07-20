ccb1cb2cf1a567e5ba9ec17d4fa5178d5b3d2261
lucile <lucile.thou@mybeeye.com>
2021-07-19 22:18:59 -0400

    setup server side

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,15 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+
+        }
+    }
+}
+
+module.exports = resolvers;
