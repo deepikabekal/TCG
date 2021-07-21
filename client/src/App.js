e3f275eb9c92480a97f4f94658e23ecc418dbe0b
Deepika Bekal <deepika.bekal2@gmail.com>
2021-07-21 15:21:17 +0400

    No match component added, components imported to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from "./Components/Footer";
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import Profile from './pages/Profile';
+import ArtForSale from './pages/ArtForSale';
+import Contact from './pages/Contact';
+import Gallery from './pages/Gallery';
 
 
 //connect to server
@@ -17,13 +25,18 @@ function App() {
         <div className='flex-column justify-flex-start min-100-vh'>
           <Header />
           <div className='container'>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/signup' component={Signup} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path = '/profile' component={Profile} />
-            <Route exact path='/artforsale' component={ArtForSale} />
-            <Route exact path='/contact' component={Contact} />
-            <route exact path='/gallery' component={Gallery} />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/signup' component={Signup} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path = '/profile' component={Profile} />
+              <Route exact path='/artforsale' component={ArtForSale} />
+              <Route exact path='/contact' component={Contact} />
+              <route exact path='/gallery' component={Gallery} />
+
+              {/* if no route macthes then show error message to user */}
+              <Route component={NoMatch} />
+            </Switch>
           </div>
           <Footer />
         </div>
