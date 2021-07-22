449a3d19abf9f6b6c4928c20f5daf67cf6eef966
lucile <developer.lvt@gmail.com>
2021-07-22 16:15:23 -0400

    profile page wip

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,17 +1,13 @@
 import React, { useState } from 'react';
-import Axios from "axios";
 
 const Profile = () => {
-
-    const [imageSelected, setImageSelected] = useState();
-     const [fileInputState, setFileInputState] = useState()
-    const [selectedFile, setSelectedFile] = useState()
+    const [fileInputState, setFileInputState] = useState()
     const [previewSource, setPreviewSource] = useState()
 
-    // handle form add art submit
+    // handle form add art submit and image post to cloudinary
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(!previewSource) return;
+        if (!previewSource) return;
         uploadImage(previewSource);
 
     };
@@ -20,6 +16,7 @@ const Profile = () => {
         console.log(e.target.name)
     }
 
+    //listens for file name change input
     const handleFileInputChange = (e) => {
         const file = e.target.files[0];
         previewFile(file);
@@ -29,67 +26,55 @@ const Profile = () => {
     const previewFile = (file) => {
         const reader = new FileReader();
         reader.readAsDataURL(file);
-        reader.onloadend = () =>{
+        reader.onloadend = () => {
             setPreviewSource(reader.result)
         }
     }
 
-/*     const uploadImage = () => {
-        console.log(imageSelected)
-        const formData = new FormData()
-        formData.append("file", imageSelected);
-        formData.append("upoload_preset", "aaxfejm1");
-        console.log(formData.files)
-
-        Axios.post("https://api.cloudinary.com/v1_1/dg14lkrkd/upload", formData
-        ).then((response) => 
-            { console.log(response) });
-    };   */    
-    
-    const uploadImage = async (base64EncodedImage) =>{
+    const uploadImage = async (base64EncodedImage) => {
         console.log(base64EncodedImage);
-        try{
+        try {
             await fetch("/api/upload", {
-                method:"POST", 
-                body: JSON.stringify({data: base64EncodedImage}),
-                headers: {"Content-type":"application/json" }
+                method: "POST",
+                body: JSON.stringify({ data: base64EncodedImage }),
+                headers: { "Content-type": "application/json" }
             }
-                )
+            )
 
-        } catch(error) {
+        } catch (error) {
             console.error(error)
         }
     }
 
     return (
         <div>
-        <form id="add-art" onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="title">Art Title</label>
-                <input type="text" name="title" onBlur={handleChange}></input>
-            </div>
-            <div>
-                <label htmlFor="description">Description</label>
-                <input type="text" name="description" onBlur={handleChange}></input>
-            </div>
-            <div>
-                <label htmlFor="medium">Medium</label>
-                <input type="text" name="description" onBlur={handleChange}></input>
-            </div>
-            <div>
-                <label htmlFor="sellingprice">Sell Price</label>
-                <input type="text" name="sellingprice" onBlur={handleChange}></input>
-            </div>
-            <div>
-                <label htmlFor="imageupload">Upload Art image</label>
-                <input type="file" name="imageupload" value={fileInputState} onChange={handleFileInputChange}></input>
-            </div>
+            <form id="add-art" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="title">Art Title</label>
+                    <input type="text" name="title" onBlur={handleChange}></input>
+                </div>
+                <div>
+                    <label htmlFor="description">Description</label>
+                    <input type="text" name="description" onBlur={handleChange}></input>
+                </div>
+                <div>
+                    <label htmlFor="medium">Medium</label>
+                    <input type="text" name="description" onBlur={handleChange}></input>
+                </div>
+                <div>
+                    <label htmlFor="sellingprice">Sell Price</label>
+                    <input type="text" name="sellingprice" onBlur={handleChange}></input>
+                </div>
+                <div>
+                    <label htmlFor="imageupload">Upload Art image</label>
+                    <input type="file" name="imageupload" value={fileInputState} onChange={handleFileInputChange}></input>
+                </div>
 
-            <button data-testid="button" type="submit" onClick={uploadImage}>
-                Submit
-            </button>
-        </form>
-        {previewSource && (<img src={previewSource}/>)}
+                <button data-testid="button" type="submit" onClick={uploadImage}>
+                    Submit
+                </button>
+            </form>
+            {previewSource && (<img style={{height:"200px"}}src={previewSource} />)}
         </div>
     )
 };
